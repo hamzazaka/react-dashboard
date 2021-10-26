811cc23f0d2946eb41155ffe8bb3cec1287aacd7
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-26 14:09:41 -0400

    topbar is completed

--- react-dashboard/src/components/Topbar.js
+++ react-dashboard/src/components/Topbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NotificationsNone} from '@material-ui/icons';
+import {NotificationsNone, Language, Settings} from '@material-ui/icons';
 
 export default function Topbar() {
     return (
@@ -12,10 +12,17 @@ export default function Topbar() {
 
                 <div className="topRight">
                     <div className="topbarIconsContainer">
-                        <NotificationsNone />
+                        <NotificationsNone/>
                         <span className='topIconBadge'>2</span>
-
                     </div>
+                     <div className="topbarIconsContainer">
+                        <Language/>
+                        <span className='topIconBadge'>2</span>
+                    </div>
+                     <div className="topbarIconsContainer">
+                        <Settings/>
+                    </div>
+                    <img src="https://media.istockphoto.com/photos/learn-to-love-yourself-first-picture-id1291208214?b=1&k=20&m=1291208214&s=170667a&w=0&h=sAq9SonSuefj3d4WKy4KzJvUiLERXge9VgZO-oqKUOo=" alt="" className="topAvatar" />
                 </div>
             </div>
         </div>
