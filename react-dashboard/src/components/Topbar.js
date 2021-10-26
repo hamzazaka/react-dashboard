53e374e38f7a21f57f650bb40e3cebc24658beb6
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-26 13:40:46 -0400

    working on the topbar

--- react-dashboard/src/components/Topbar.js
+++ react-dashboard/src/components/Topbar.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {NotificationsNone} from '@material-ui/icons';
 
 export default function Topbar() {
     return (
-        <div>
-            <h1>helllo fro topbar</h1>
+        <div className='topbar'>
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <div className="logo">lamaaadmin</div>
+                </div>
+
+
+                <div className="topRight">
+                    <div className="topbarIconsContainer">
+                        <NotificationsNone />
+                        <span className='topIconBadge'>2</span>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
