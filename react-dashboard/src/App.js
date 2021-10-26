43a8e476d110a14ca7e0d24f7b9db1d6227cc461
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-26 15:49:07 -0400

    working on the sidebar

--- react-dashboard/src/App.js
+++ react-dashboard/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Topbar from './components/Topbar';
+import SideBar from './components/SideBar';
 
 function App() {
   return (
     <div className="app">
       <Topbar/>
+      <div className="container">
+        <SideBar/>
+        <div className="others">
+          <h2>others</h2>
+          </div> 
+      </div>
       <h1>hello world</h1>
     </div>
   );
