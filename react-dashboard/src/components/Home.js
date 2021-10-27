88062baaea4655f481a7eaf769f2f9bba4925ee5
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-27 11:36:31 -0400

    working on the widgets of users

--- react-dashboard/src/components/Home.js
+++ react-dashboard/src/components/Home.js
@@ -2,12 +2,19 @@ import React from 'react'
 import Chart from './Chart'
 import Feathedinfo from './Feathedinfo';
 import { Userdata } from './ChartData';
+import WidgetSm from './WidgetSm';
+import WidgetLg from './WidgetLg';
 
 export default function Home() {
     return (
         <div className='home'>
             <Feathedinfo/>
             <Chart data={Userdata} title='User Analytics' grid dataKey='Active User'/>
+            <div className="homeWidgets">
+                <WidgetSm/>
+                <WidgetLg/>
+
+            </div>
         </div>
     )
 }
---
+++ react-dashboard/src/components/WidgetSm.js
@@ -0,0 +1,62 @@
+import { Visibility } from '@material-ui/icons'
+import React from 'react'
+
+export default function WidgetSm() {
+    return (
+        <div className='widgetSm'>
+            <span className="widgetSmTitle">New Join Members</span>
+            <ul className="widgetSmList">
+                <li className="widgetSmListItem">
+                    <img  className='widgetSmImg' src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJlYXV0eSUyMHBpY3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt=""/>
+                    <div className="widgetSmUser">
+                    <span className='widgetSmUsername'>Anna Keller</span>
+                    <span className='widgetSmUserTitle'>Software Engineer</span>
+                    </div>
+                    <button className="sidgetSmButton">
+                        <Visibility/> 
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img  className='widgetSmImg' src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJlYXV0eSUyMHBpY3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt=""/>
+                    <div className="widgetSmUser">
+                    <span className='widgetSmUsername'>Anna Keller</span>
+                    <span className='widgetSmUserTitle'>Software Engineer</span>
+                    </div>
+                    <button className="sidgetSmButton">
+                        <Visibility/> 
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img  className='widgetSmImg' src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJlYXV0eSUyMHBpY3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt=""/>
+                    <div className="widgetSmUser">
+                    <span className='widgetSmUsername'>Anna Keller</span>
+                    <span className='widgetSmUserTitle'>Software Engineer</span>
+                    </div>
+                    <button className="sidgetSmButton">
+                        <Visibility/> 
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img  className='widgetSmImg' src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJlYXV0eSUyMHBpY3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt=""/>
+                    <div className="widgetSmUser">
+                    <span className='widgetSmUsername'>Anna Keller</span>
+                    <span className='widgetSmUserTitle'>Software Engineer</span>
+                    </div>
+                    <button className="sidgetSmButton">
+                        <Visibility/> 
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img  className='widgetSmImg' src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJlYXV0eSUyMHBpY3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt=""/>
+                    <div className="widgetSmUser">
+                    <span className='widgetSmUsername'>Anna Keller</span>
+                    <span className='widgetSmUserTitle'>Software Engineer</span>
+                    </div>
+                    <button className="sidgetSmButton">
+                        <Visibility/> 
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
+}
