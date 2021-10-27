8ae67264aa5feb2fd6390a899ab7655f9908f73c
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-27 02:33:10 -0400

    working on charts

--- react-dashboard/src/components/Chart.js
+++ react-dashboard/src/components/Chart.js
@@ -6,50 +6,62 @@ export default function Chart() {
     const data = [
   {
     name: 'Jan',
+    NewUser: Math.trunc(Math.random()*50),
     "Active User" : Math.trunc(Math.random()*230)
   },
   {
     name: 'Feb',
+    NewUser: Math.trunc(Math.random()*50),
     "Active User" : Math.trunc(Math.random()*230),
   },
   {
     name: 'Mar',
+    NewUser: Math.trunc(Math.random()*50),
     "Active User" : Math.trunc(Math.random()*230),
   },
   {
     name: 'Apr',
+    NewUser: Math.trunc(Math.random()*50),
     "Active User" : Math.trunc(Math.random()*230),
   },
   {
     name: 'May',
+    NewUser: Math.trunc(Math.random()*50),
     "Active User" : Math.trunc(Math.random()*230),
   },
   {
     name: 'Jun',
+    NewUser: Math.trunc(Math.random()*50),
     "Active User" : Math.trunc(Math.random()*230),
   },
   {
     name: 'Jul',
+    NewUser: Math.trunc(Math.random()*50),
     "Active User" : Math.trunc(Math.random()*300),
   },
   {
     name: 'Aug',
+    NewUser: Math.trunc(Math.random()*50),
     "Active User" : Math.trunc(Math.random()*230),
   },
   {
     name: 'Sep',
+    NewUser: Math.trunc(Math.random()*50),
     "Active User" : Math.trunc(Math.random()*230),
   },
   {
     name: 'Oct',
+    NewUser: Math.trunc(Math.random()*50),
     "Active User" : Math.trunc(Math.random()*230),
   },
   {
     name: 'Nov',
+    NewUser: Math.trunc(Math.random()*50),
     "Active User" : Math.trunc(Math.random()*230),
   },
   {
     name: 'Dec',
+    NewUser: Math.trunc(Math.random()*50),
     "Active User" : Math.trunc(Math.random()*230),
   },
 
@@ -63,7 +75,9 @@ export default function Chart() {
             <ResponsiveContainer width='100%' aspect={4/1}>
                 <LineChart data={data}>
                     <XAxis dataKey="name"  stroke='#5550bd'/>
+                    <YAxis stroke='#5550bd'/>
                     <Line type="monotone" dataKey="Active User" stroke="#5550bd" />
+                    <Line type="monotone" dataKey="NewUser" stroke="green" />
                     <Tooltip />
                     <CartesianGrid stroke='#e0dfdf' strokeDasharray='5 5'/>
                     </LineChart>
